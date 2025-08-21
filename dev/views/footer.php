2693031f65a69928dfb631ee8be0ec640fe09363
Raisa Silva <raisa.gagliano@gmail.com>
2025-08-21 20:24:10 -0300

    teste teste

---
+++ dev/views/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center text-lg-start" style="background: linear-gradient(90deg, #465efb 0%, #c2ffd8 100%);">
+    <div class="text-center text-white p-1">
+        <p>© 2025 <?php echo NOME?> - Todos os direitos reservados.</p>
+    </div>
+</footer>
